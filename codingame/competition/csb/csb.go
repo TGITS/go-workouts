c145636da422ad2d0f7969b6d1aabb6dd89871b7
TGITS <thegeekintheshell@gmail.com>
2017-08-21 09:29:07 +0200

    Refactoring (WIP) sur CSB

--- codingame/competition/csb/csb.go
+++ codingame/competition/csb/csb.go
@@ -20,149 +20,18 @@ const (
 
 // ToRadians convert the parameters corresponding to an angle in degrees int an angle in radians.
 // radians  = degrees * PI / 180
-func ToRadians(degrees int) int { return round(float64(degrees) * math.Pi / 180.0) }
+func ToRadians(degrees float64) float64 { return float64(degrees) * math.Pi / 180.0 }
 
 // ToDegrees convert the parameters corresponding to an angle in radians int an angle in degrees.
 // degreess = radians * 180 / PI
-func ToDegrees(radians int) int { return round(float64(radians) * 180.0 / math.Pi) }
+func ToDegrees(radians float64) float64 { return float64(radians) * 180.0 / math.Pi }
 
-//Round a float64 and convert it to an int32
-func round(val float64) int {
+// Round rounds a float64, converts it to an int and returns it
+func Round(val float64) int {
 	if val < 0 {
 		return int(val - 0.5)
 	}
 	return int(val + 0.5)
-}
-
-//Representing the coordinate of a Game Element
-type Point struct {
-	X, Y int
-}
-
-//Calculating the Distance
-func (p1 *Point) Distance(p2 *Point) int {
-	x := p2.X - p1.X
-	y := p2.Y - p1.Y
-	return int((round(math.Sqrt(float64(x*x + y*y)))))
-}
-
-//representing a Pod in CSB
-type Pod struct {
-	Point
-	AngleToCP    int
-	DistanceToCp int
-	Thrust       bool
-	Shield       int // with possible value 0 (not recently used or more than 3), 3 (just used), 2 (used 1 turn ago), 1 (used 2 turns ago)
-}
-
-type Checkpoint struct {
-	Point
-}
-
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-func main() {
-
-	// checkpoints := make(map[complex128]struct{})
-	var checkpoint complex128
-	var pod complex128
-	var opponentPod complex128
-	var xTarget, yTarget, thrust string
-	// var checkpointAngle, podAngle, opponentPodAngle, angleBetweenPods float64
-	turns := 0
-	boostUsed := new(bool)
-	*boostUsed = false
-	// thrustByTurn := make(map[int]string)
-	// thrustByTurn[turns] = "0"
-	for {
-		// nextCheckpointX: x position of the next check point
-		// nextCheckpointY: y position of the next check point
-		// nextCheckpointDist: distance to the next checkpoint
-		// nextCheckpointAngle: angle between your pod orientation and the direction of the next checkpoint
-		var x, y, nextCheckpointX, nextCheckpointY, nextCheckpointDist, nextCheckpointAngle int
-		fmt.Scan(&x, &y, &nextCheckpointX, &nextCheckpointY, &nextCheckpointDist, &nextCheckpointAngle)
-
-		var opponentX, opponentY int
-		fmt.Scan(&opponentX, &opponentY)
-
-		turns++
-		pod = complex(float64(x), float64(y))
-		checkpoint = complex(float64(nextCheckpointX), float64(nextCheckpointY))
-		opponentPod = complex(float64(opponentX), float64(opponentY))
-		//distanceBetweenPods := int(cmplx.Abs(opponentPod - pod))
-		// if _, ok := checkpoints[checkpoint]; ok != true {
-		// 	checkpoints[checkpoint] = struct{}{}
-		// }
-
-		// fmt.Fprintln(os.Stderr, "Debug messages...")
-		// fmt.Fprintf(os.Stderr, "Turn number : %d\n", turns)
-		fmt.Fprintf(os.Stderr, "Pod coordinates : %.1f\n", pod)
-		fmt.Fprintf(os.Stderr, "Opponent pod coordinates : %.1f\n", opponentPod)
-		fmt.Fprintf(os.Stderr, "Checkpoint coordinates : %.1f\n", checkpoint)
-		fmt.Fprintf(os.Stderr, "Angle in degrees between pod and next checkpoint : %d\n", nextCheckpointAngle)
-		// fmt.Fprintf(os.Stderr, "List of checkpoints : %v\n", checkpoints)
-		// fmt.Fprintf(os.Stderr, "Number of checkpoints : %v\n", len(checkpoints))
-		// fmt.Fprintf(os.Stderr, "Boost use ? : %t\n", *boostUsed)
-		// fmt.Fprintf(os.Stderr, "Distance from pod to next checkpoint : %d\n", nextCheckpointDist)
-		// fmt.Fprintf(os.Stderr, "Calculated distance between pod and checkpoint : %f\n", cmplx.Abs(checkpoint-pod))
-		//fmt.Fprintf(os.Stderr, "Calculated distance between pod and opponent pod : %d\n", distanceBetweenPods)
-
-		// _, checkpointAngle = cmplx.Polar(checkpoint)
-		// _, podAngle = cmplx.Polar(pod)
-		// _, opponentPodAngle = cmplx.Polar(opponentPod)
-		// fmt.Fprintf(os.Stderr, "Calculated phases in radians for checkpoint, pod and opponentPod : %.1f %.1f %.1f\n", checkpointAngle, podAngle, opponentPodAngle)
-		// _, angleBetweenPods = cmplx.Polar((pod - checkpoint) / (opponentPod - checkpoint))
-		// fmt.Fprintf(os.Stderr, "Calculated angle in radians between the 2 vectors (Checkpoint, Pod) and (Checkpoint, OpponentPod) : %.3f\n", angleBetweenPods)
-		// fmt.Fprintf(os.Stderr, "Calculated angle in degrees between the 2 vectors (Checkpoint, Pod) and (Checkpoint, OpponentPod) : %.3f\n", ToDegrees(angleBetweenPods))
-		//ToDegrees
-		// You have to output the target position
-		// followed by the power (0 <= thrust <= 100) or "BOOST" or "SHIELD"
-		// i.e.: "x y thrust"
-		xTarget, yTarget, thrust = computeValues(checkpoint, nextCheckpointDist, nextCheckpointAngle, pod, opponentPod, boostUsed, turns)
-		// thrustByTurn[turns] = thrust
-		fmt.Printf("%s %s %s\n", xTarget, yTarget, thrust)
-	}
-}
-
-func computeValues(checkpoint complex128, nextCheckpointDistance int, nextCheckpointAngle int, pod complex128, opponentPod complex128, boostUsed *bool, turns int) (string, string, string) {
-	target, distance, angle := computeTarget(checkpoint, nextCheckpointDistance, nextCheckpointAngle, pod)
-	return strconv.Itoa(int(real(target))), strconv.Itoa(int(imag(target))), computeAction(target, distance, angle, pod, opponentPod, boostUsed, turns)
-	//return strconv.Itoa(int(real(checkpoint))), strconv.Itoa(int(imag(checkpoint))), computeAction(checkpoint, nextCheckpointDistance, nextCheckpointAngle, pod, opponentPod, boostUsed, turns)
-}
-
-/*
-computeAction return "BOOST", "SHIELD" or the thrust value
-*/
-func computeAction(checkpoint complex128, nextCheckpointDistance int, nextCheckpointAngle int, pod complex128, opponentPod complex128, boostUsed *bool, turns int) string {
-	// if !*boostUsed && turns > MaxLoopBeforeBoost && math.Abs(float64(nextCheckpointAngle)) < 10 && nextCheckpointDistance > 4000 {
-	if !*boostUsed && math.Abs(float64(nextCheckpointAngle)) < 10 && nextCheckpointDistance > 4000 {
-		*boostUsed = true
-		return "BOOST"
-	}
-	return computeThrust(checkpoint, nextCheckpointDistance, nextCheckpointAngle, pod, opponentPod, boostUsed, turns)
-}
-
-/*
-computeThrust compute the thrust value
-*/
-func computeThrust(checkpoint complex128, nextCheckpointDistance int, nextCheckpointAngle int, pod complex128, opponentPod complex128, boostUsed *bool, turns int) string {
-	switch {
-	case math.Abs(float64(nextCheckpointAngle)) >= 90:
-		return "0"
-	case math.Abs(float64(nextCheckpointAngle)) >= 72:
-		return "20"
-	case math.Abs(float64(nextCheckpointAngle)) >= 54:
-		return "40"
-	case math.Abs(float64(nextCheckpointAngle)) >= 36:
-		return "60"
-	case math.Abs(float64(nextCheckpointAngle)) >= 18:
-		return "80"
-	default:
-		return "100"
-	}
 }
 
 func computeAngle(a complex128, b complex128, hypothenuse float64) (float64, float64, float64) {
@@ -174,13 +43,58 @@ func computeAngle(a complex128, b complex128, hypothenuse float64) (float64, flo
 	return cosTheta, sinTheta, theta
 }
 
-func computeTarget(checkpoint complex128, nextCheckpointDistance int, nextCheckpointAngle int, pod complex128) (complex128, int, int) {
+// TurnInfo is a struct that gather all the game information of a turn
+type TurnInfo struct {
+	Checkpoint  complex128
+	PlayerPod   complex128
+	OpponentPod complex128
+	Angle       int //Angle between the player pod and the next Checkpoint in degrees
+	Distance    int //Distance between the player pod and the next Checkpoint
+
+}
+
+// NewTurnInfo to create and initialize a new TurnInfo (a pointer to TurnInfo)
+func NewTurnInfo(x, y, nextCheckpointX, nextCheckpointY, nextCheckpointDist, nextCheckpointAngle, opponentX, opponentY int) *TurnInfo {
+	turnInfo := new(TurnInfo)
+	turnInfo.Checkpoint = complex(float64(nextCheckpointX), float64(nextCheckpointY))
+	turnInfo.PlayerPod = complex(float64(x), float64(y))
+	turnInfo.OpponentPod = complex(float64(opponentX), float64(opponentY))
+	turnInfo.Angle = nextCheckpointAngle
+	turnInfo.Distance = nextCheckpointDist
+	return turnInfo
+}
+
+/*
+computeThrust compute the thrust value
+*/
+func (turnInfo *TurnInfo) computeThrust() string {
+	switch {
+	case turnInfo.Distance < 1000:
+		return "20"
+	case turnInfo.Distance < 600:
+		return "00"
+	case math.Abs(float64(turnInfo.Angle)) >= 90:
+		return "0"
+	case math.Abs(float64(turnInfo.Angle)) >= 72:
+		return "20"
+	case math.Abs(float64(turnInfo.Angle)) >= 54:
+		return "40"
+	case math.Abs(float64(turnInfo.Angle)) >= 36:
+		return "60"
+	case math.Abs(float64(turnInfo.Angle)) >= 18:
+		return "80"
+	default:
+		return "100"
+	}
+}
+
+func (turnInfo *TurnInfo) computeTarget() (complex128, int, int) {
 	// intermediate := complex(real(pod), imag(checkpoint))
 	// hypothenuse := cmplx.Abs(pod - checkpoint)
 	// cosTheta := cmplx.Abs(intermediate-checkpoint) / hypothenuse
 	// sinTheta := cmplx.Abs(pod-intermediate) / hypothenuse
 	//cosTheta, sinTheta, theta := computeAngle(pod, checkpoint, float64(nextCheckpointDistance))
-	cosTheta, sinTheta, _ := computeAngle(pod, checkpoint, float64(nextCheckpointDistance))
+	cosTheta, sinTheta, _ := computeAngle(turnInfo.PlayerPod, turnInfo.Checkpoint, float64(turnInfo.Distance))
 	// fmt.Fprintf(os.Stderr, "cosTheta : %.1f\n", cosTheta)
 	// fmt.Fprintf(os.Stderr, "sinTheta : %.1f\n", sinTheta)
 	// fmt.Fprintf(os.Stderr, "theta : %.1f\n", theta)
@@ -191,27 +105,27 @@ func computeTarget(checkpoint complex128, nextCheckpointDistance int, nextCheckp
 	targetsByDistance := make(map[float64]complex128)
 	targetsByAngle := make(map[float64]complex128)
 
-	distances[0] = float64(nextCheckpointDistance)
-	angles[0] = math.Abs(float64(nextCheckpointAngle))
-	targetsByDistance[distances[0]] = checkpoint
-	targetsByAngle[angles[0]] = checkpoint
+	distances[0] = float64(turnInfo.Distance)
+	angles[0] = math.Abs(float64(turnInfo.Angle))
+	targetsByDistance[distances[0]] = turnInfo.Checkpoint
+	targetsByAngle[angles[0]] = turnInfo.Checkpoint
 	index := 1
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 2; j++ {
 			if i%2 != 0 {
-				xTarget = real(checkpoint) - PodRadius*cosTheta
+				xTarget = real(turnInfo.Checkpoint) - PodRadius*cosTheta
 			} else {
-				xTarget = real(checkpoint) + PodRadius*cosTheta
+				xTarget = real(turnInfo.Checkpoint) + PodRadius*cosTheta
 			}
 
 			if j%2 != 0 {
-				yTarget = imag(checkpoint) + PodRadius*sinTheta
+				yTarget = imag(turnInfo.Checkpoint) + PodRadius*sinTheta
 			} else {
-				yTarget = imag(checkpoint) - PodRadius*sinTheta
+				yTarget = imag(turnInfo.Checkpoint) - PodRadius*sinTheta
 			}
 			target := complex(xTarget, yTarget)
-			distances[index] = cmplx.Abs(pod - target)
-			_, _, angle := computeAngle(pod, target, distances[index])
+			distances[index] = cmplx.Abs(turnInfo.PlayerPod - target)
+			_, _, angle := computeAngle(turnInfo.PlayerPod, target, distances[index])
 			targetsByDistance[distances[index]] = target
 			angles[index] = math.Abs(angle)
 			targetsByAngle[angles[index]] = target
@@ -226,4 +140,153 @@ func computeTarget(checkpoint complex128, nextCheckpointDistance int, nextCheckp
 	fmt.Fprintf(os.Stderr, "targetsByAngle : %v\n", targetsByAngle)
 	//return targetsByDistance[distances[0]]
 	return targetsByAngle[angles[0]], int(distances[0]), int(angles[0])
+}
+
+// GameInfo is a struct that gather all the global game information relative to the game
+type GameInfo struct {
+	Checkpoints []complex128
+	History     map[int]*TurnInfo
+	Boost       bool //True if a boost is available, false if the thrust of the player pod has already been used
+	Shield      int  //To mark if the shield has been used. With possible value 0 (not recently used or more than 3 turns ; the initial value), 3 (just used), 2 (used 1 turn ago), 1 (used 2 turns ago)
+	Turn        int  // 0 is the initial value
+	Lap         int  //The lap in which we are. This value has to be computed
+
+}
+
+// NewGameInfo To create and initialize a new GameInfo (a pointer to GameInfo)
+// Essentially the game board for CSB in silver league is a slice of checkpoints
+// This slice is poupulated turns by turns, we know we are in the second lap when we cannot had a new CP
+// We know we are in the third lap when we have reached the first checkpoint
+func NewGameInfo() *GameInfo {
+	gameInfo := new(GameInfo)
+	gameInfo.Boost = true
+	gameInfo.Shield = 3
+	gameInfo.Turn = 0
+	gameInfo.Lap = 0
+	gameInfo.Checkpoints = make([]complex128, 0, 10)
+	gameInfo.History = make(map[int]*TurnInfo)
+	return gameInfo
+}
+
+// UpdateHistory is a method that had a new TurnInfo in the GameInfo. Before it update the turn count, the checkpoint list and the Lap count if necessary
+func (gameInfo *GameInfo) UpdateHistory(turnInfo *TurnInfo) {
+	gameInfo.UpdateTurn()
+	if gameInfo.Lap == 0 {
+		gameInfo.UpdateCheckpoints(turnInfo.Checkpoint)
+	}
+	gameInfo.History[gameInfo.Turn] = turnInfo
+}
+
+// PreviousTurn is a method that return the counter value of the previous turn
+func (gameInfo *GameInfo) PreviousTurn() int {
+	return gameInfo.Turn - 1
+}
+
+// NextTurn is a method that return the counter value of the next turn
+func (gameInfo *GameInfo) NextTurn() int {
+	return gameInfo.Turn + 1
+}
+
+// UpdateTurn is a method that updates the turn counter and return an int representing this new value
+func (gameInfo *GameInfo) UpdateTurn() {
+	gameInfo.Turn++
+}
+
+// UpdateCheckpoints is a method that update the list of checkpoints if necessary
+func (gameInfo *GameInfo) UpdateCheckpoints(checkpoint complex128) {
+	newCheckpoint, newLap := gameInfo.In(checkpoint)
+	if !newCheckpoint {
+		gameInfo.Checkpoints = append(gameInfo.Checkpoints, checkpoint)
+	}
+	if newLap {
+		gameInfo.Lap++
+	}
+}
+
+// In is a method that checks if the complex number is in the slice Checkpoints and if it is a new turn
+func (gameInfo *GameInfo) In(complex complex128) (bool, bool) {
+	for index, value := range gameInfo.Checkpoints {
+		if complex == value {
+			if index == 0 {
+				return true, true
+			}
+			return true, false
+		}
+	}
+	return false, false
+}
+
+func (gameInfo *GameInfo) updateBoost() {
+	gameInfo.Boost = false
+}
+
+func (gameInfo *GameInfo) getCurrentTurnInfo() *TurnInfo {
+	return gameInfo.History[gameInfo.Turn]
+}
+
+// DumpInfo is a method to dump the information on stderr
+func (gameInfo *GameInfo) DumpInfo() {
+	fmt.Fprintf(os.Stderr, "Turn : %d\n", gameInfo.Turn)
+	fmt.Fprintf(os.Stderr, "Lap : %d\n", gameInfo.Lap)
+	fmt.Fprintf(os.Stderr, "Checkpoints : %v\n", gameInfo.Checkpoints)
+	fmt.Fprintf(os.Stderr, "Boost use ? : %t\n", gameInfo.Boost)
+	turnInfo := gameInfo.getCurrentTurnInfo()
+	fmt.Fprintf(os.Stderr, "Checkpoint coordinates : %.1f\n", turnInfo.Checkpoint)
+	fmt.Fprintf(os.Stderr, "Palyer Pod coordinates : %.1f\n", turnInfo.PlayerPod)
+	fmt.Fprintf(os.Stderr, "Opponent pod coordinates : %.1f\n", turnInfo.OpponentPod)
+	fmt.Fprintf(os.Stderr, "Angle in degrees between pod and next checkpoint : %d\n", turnInfo.Angle)
+	fmt.Fprintf(os.Stderr, "Distance from pod to next checkpoint : %d\n", turnInfo.Distance)
+}
+
+func (gameInfo *GameInfo) computeValues() (string, string, string) {
+	turnInfo := gameInfo.getCurrentTurnInfo()
+	target, _, _ := turnInfo.computeTarget()
+	return strconv.Itoa(int(real(target))), strconv.Itoa(int(imag(target))), gameInfo.computeAction()
+	//return strconv.Itoa(int(real(checkpoint))), strconv.Itoa(int(imag(checkpoint))), computeAction(checkpoint, nextCheckpointDistance, nextCheckpointAngle, pod, opponentPod, boostUsed, turns)
+}
+
+/*
+computeAction return "BOOST", "SHIELD" or the thrust value
+*/
+func (gameInfo *GameInfo) computeAction() string {
+	turnInfo := gameInfo.getCurrentTurnInfo()
+	if gameInfo.Boost && math.Abs(float64(turnInfo.Angle)) < 10 && turnInfo.Distance > 4000 {
+		gameInfo.updateBoost()
+		return "BOOST"
+	}
+	return turnInfo.computeThrust()
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+func main() {
+
+	var xTarget, yTarget, thrust string
+	gameInfo := NewGameInfo()
+	for {
+		// nextCheckpointX: x position of the next check point
+		// nextCheckpointY: y position of the next check point
+		// nextCheckpointDist: distance to the next checkpoint
+		// nextCheckpointAngle: angle between your pod orientation and the direction of the next checkpoint
+		var x, y, nextCheckpointX, nextCheckpointY, nextCheckpointDist, nextCheckpointAngle int
+		fmt.Scan(&x, &y, &nextCheckpointX, &nextCheckpointY, &nextCheckpointDist, &nextCheckpointAngle)
+
+		var opponentX, opponentY int
+		fmt.Scan(&opponentX, &opponentY)
+
+		turnInfo := NewTurnInfo(x, y, nextCheckpointX, nextCheckpointY, nextCheckpointDist, nextCheckpointAngle, opponentX, opponentY)
+		gameInfo.UpdateHistory(turnInfo)
+
+		gameInfo.DumpInfo()
+
+		// You have to output the target position
+		// followed by the power (0 <= thrust <= 100) or "BOOST" or "SHIELD"
+		// i.e.: "x y thrust"
+		xTarget, yTarget, thrust = gameInfo.computeValues()
+		// thrustByTurn[turns] = thrust
+		fmt.Printf("%s %s %s\n", xTarget, yTarget, thrust)
+	}
 }
